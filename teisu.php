561cbcde0394b16aeb6c465a9bb8b2ea02985b1b
mmr.K <lmimsra07@gmail.com>
2018-02-18 12:31:17 +0900

    add define

---
+++ teisu.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mmr
+ * Date: 2018/02/18
+ * Time: 12:28
+ */
+
+define("MY_NAME","taro");
+
+echo MY_NAME;
+
+var_dump(__LINE__);
+var_dump(__FILE__);
+var_dump(__DIR__);
